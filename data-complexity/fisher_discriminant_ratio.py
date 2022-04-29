0f244bb18e8d0a5583bb3db55a9f23b259314ba9
wtaisner <taisner.witold@gmail.com>
2022-04-29 17:35:38 +0200

    init implementation

---
+++ data-complexity/fisher_discriminant_ratio.py
@@ -0,0 +1,19 @@
+import collections
+
+import pandas as pd
+import numpy as np
+
+
+def fisher_discriminant_ratio(features: pd.DataFrame, target: pd.DataFrame, ) -> float:
+    """
+
+    :param features:
+    :param target:
+    :return:
+    """
+
+
+
+# for tests
+if __name__ == '__main__':
+    pass
