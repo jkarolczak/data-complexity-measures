604e62f87c0dfebba2e153430e72685bfb3eb25e
annprzy <anneprzybylowska@gmail.com>
2022-05-02 16:26:33 +0200

    add f1 implementation

--- data-complexity/fisher_discriminant_ratio.py
+++ data-complexity/fisher_discriminant_ratio.py
@@ -11,6 +11,28 @@ def fisher_discriminant_ratio(features: pd.DataFrame, target: pd.DataFrame, ) ->
     :param target:
     :return:
     """
+    target_name = target.columns[0]
+    n_cj = target.groupby(target_name)[target_name].count()
+
+    assert len(list(n_cj.index)) >= 2, "Can't compute F1 for one class"
+
+    df = features.join(target)
+    mean_c = df.groupby(target_name).mean()
+    mean_f = features.mean()
+
+    nominator = n_cj.T * ((mean_c - mean_f) ** 2).T
+    nominator = nominator.T.sum()
+
+    mean_c[target_name] = 0
+    denominator = df.apply(lambda x: (x - mean_c.loc[x[target_name]]) ** 2, axis=1)
+    denominator = denominator.drop(columns=[target_name]).sum()
+
+    r = nominator / denominator
+    r[~np.isfinite(r)] = 0
+
+    assert np.all(r > 0), 'All the instances are equal except for the class.'
+
+    return 1 / (1 + np.max(r))
 
 
 
